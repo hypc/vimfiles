8b0a1d8d5e4d4b3a6dccc167e288c5454bd2fb4e
hypc <h_yp00@163.com>
2018-10-30 11:00:38 +0800

    update install.simple.sh

--- install.simple.sh
+++ install.simple.sh
@@ -1,6 +1,5 @@
 #!/bin/env bash
 
-git clone https://github.com/hypc/vimfiles.git ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --recurse-submodules https://github.com/hypc/vimfiles.git ~/.vim
 ln -s ~/.vim/vimrc.simple ~/.vimrc && vim +PluginInstall +qall
 
